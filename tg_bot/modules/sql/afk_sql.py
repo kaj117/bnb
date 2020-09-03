ad8a50087af6fa8fe8085944c2e7e9942541c688
kaj117 <70688258+kaj117@users.noreply.github.com>
2020-09-03 14:21:43 +0530

    Update afk_sql.py

--- tg_bot/modules/sql/afk_sql.py
+++ tg_bot/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, Integer
 
-from tg_bot.modules.sql import BASE, SESSION
+from kmac_bot.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
