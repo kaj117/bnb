d054fa28ded20ff37490076a0a2f14cf9a90b27a
kaj117 <70688258+kaj117@users.noreply.github.com>
2020-09-03 14:16:41 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -9,18 +9,18 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryH
 from telegram.ext.dispatcher import run_async, DispatcherHandlerStop
 from telegram.utils.helpers import escape_markdown
 
-from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK, CERT_PATH, PORT, URL, LOGGER, \
+from kmac_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK, CERT_PATH, PORT, URL, LOGGER, \
     ALLOW_EXCL
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
-from tg_bot.modules import ALL_MODULES
-from tg_bot.modules.helper_funcs.chat_status import is_user_admin
-from tg_bot.modules.helper_funcs.misc import paginate_modules
+from kmac_bot.modules import ALL_MODULES
+from kmac_bot.modules.helper_funcs.chat_status import is_user_admin
+from kmac_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
 
-(https://heroku.com/deploy?template=https://github.com/TGExplore/Marie_Chechi2.0) 
+(https://heroku.com/deploy?template=hhttps://github.com/kaj117/bnb) 
 
 Using standard [Marie](http://telegram.dog/BanhammerMarie_bot) is always gonna be easier.
 [Marie](http://telegram.dog/BanhammerMarie_bot)
